808ba2d8a08743dadaefb37904c1dc18742e80aa
Dave Pederson <dave@t12y.com>
2019-08-16 13:42:54 -0600

    Add Makefile

--- app/Main.hs
+++ app/Main.hs
@@ -1,56 +1,51 @@
 module Main where
 
-
 import Data.Foldable (traverse_)
 import MINQ
 
-
 -- Defines a row type for top-level movie metadata.
-data Movie = Movie
-  { movieId     :: Int
-  , movieGenres :: [String]
-  , movieYear   :: Int
-  , movieTitle  :: String
-  }
+data Movie
+  = Movie
+      { movieId :: Int
+      , movieGenres :: [String]
+      , movieYear :: Int
+      , movieTitle :: String
+      }
   deriving Show
-
 
 -- Defines a row type for movie trailers.
-data Trailer = Trailer
-  { trailerId      :: Int
-  , trailerMovieId :: Int -- FK to Movie
-  , trailerURL     :: String
-  , trailerActive  :: Bool
-  }
+data Trailer
+  = Trailer
+      { trailerId :: Int
+      , trailerMovieId :: Int -- FK to Movie
+      , trailerURL :: String
+      , trailerActive :: Bool
+      }
   deriving Show
-
 
 -- Select action (or science fiction) movies.
 actionMovies :: [Movie] -> [Movie]
 actionMovies movies =
   runMINQ $
     MINQ (select_ id) -- select *
-         movies
-         (where_ $ (elem "Action" `or_` elem "Sci-Fi") . movieGenres)
-
+      movies
+      (where_ $ (elem "Action" `or_` elem "Sci-Fi") . movieGenres)
 
 -- Select id and title of action movies released in the 1980s.
 actionMovies80s :: [Movie] -> [(Int, String)]
 actionMovies80s movies =
   runMINQ $
     MINQ (select_ $ \r -> (movieId r, movieTitle r))
-         (actionMovies movies)
-         (where_ $ (`elem` [1980..1989]) . movieYear)
-
+      (actionMovies movies)
+      (where_ $ (`elem` [1980.. 1989]) . movieYear)
 
 -- Select movie name and trailer URL of movies that have at least one active trailer.
 movieTrailers :: [Movie] -> [Trailer] -> [(String, String)]
 movieTrailers movies trailers =
   runMINQ $
     MINQ (select_ $ \(m, t) -> (movieTitle m, trailerURL t))
-         (join_ movies movieId trailers trailerMovieId)
-         (where_ $ (==True) . trailerActive . snd)
-
+      (join_ movies movieId trailers trailerMovieId)
+      (where_ $ (== True) . trailerActive . snd)
 
 -- Sample movie table.
 movieTable :: [Movie]
@@ -65,7 +60,6 @@ movieTable =
   , Movie 51 ["Action"] 1990 "Die Hard 2"
   ]
 
-
 -- Sample trailer table.
 trailerTable :: [Trailer]
 trailerTable =
@@ -73,7 +67,6 @@ trailerTable =
   , Trailer 2 73 "https://www.youtube.com/watch?v=qSqVVswa420" True
   , Trailer 3 24 "https://www.youtube.com/watch?v=kGIM_yNzeUo" False
   ]
-
 
 -- Run various example MINQ queries.
 main :: IO ()
