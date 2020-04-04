7e7ca21ad73a45e70dd568829f3dd412e28da3ba
Dave Pederson <dave.pederson@gmail.com>
2020-04-03 18:48:58 -0600

    Bump stack lts version

--- app/Main.hs
+++ app/Main.hs
@@ -57,23 +57,23 @@ movieTrailers movies trailers =
 -- Sample movie table.
 movieTable :: [Movie]
 movieTable =
-  [ Movie 81 ["Action"] 1988 "Die Hard"
-  , Movie 72 ["Action"] 1987 "Top Gun"
-  , Movie 63 ["Comedy"] 1987 "Space Balls"
-  , Movie 54 ["Action"] 2020 "Top Gun: Maverick"
-  , Movie 45 ["Action"] 1987 "Lethal Weapon"
-  , Movie 36 ["Sci-Fi"] 1987 "Predator"
-  , Movie 27 ["Sci-Fi"] 1990 "Predator 2"
-  , Movie 18 ["Action"] 1990 "Die Hard 2"
-  ]
+    [ Movie 81 ["Action"] 1988 "Die Hard"
+    , Movie 72 ["Action"] 1987 "Top Gun"
+    , Movie 63 ["Comedy"] 1987 "Space Balls"
+    , Movie 54 ["Action"] 2020 "Top Gun: Maverick"
+    , Movie 45 ["Action"] 1987 "Lethal Weapon"
+    , Movie 36 ["Sci-Fi"] 1987 "Predator"
+    , Movie 27 ["Sci-Fi"] 1990 "Predator 2"
+    , Movie 18 ["Action"] 1990 "Die Hard 2"
+    ]
 
 -- Sample trailer table.
 trailerTable :: [Trailer]
 trailerTable =
-  [ Trailer 1 81 "https://www.youtube.com/watch?v=2TQ-pOvI6Xo" Approved
-  , Trailer 2 54 "https://www.youtube.com/watch?v=qSqVVswa420" Approved
-  , Trailer 3 63 "https://www.youtube.com/watch?v=kGIM_yNzeUo" Submitted
-  ]
+    [ Trailer 1 81 "https://www.youtube.com/watch?v=2TQ-pOvI6Xo" Approved
+    , Trailer 2 54 "https://www.youtube.com/watch?v=qSqVVswa420" Approved
+    , Trailer 3 63 "https://www.youtube.com/watch?v=kGIM_yNzeUo" Submitted
+    ]
 
 -- Run various example MINQ queries.
 main :: IO ()
