3deaa260650a3915f7c5cd4132748e29eca4884e
Dave Pederson <dave.pederson@gmail.com>
2020-06-10 19:44:01 -0600

    Add test comments

--- test/unit/Spec.hs
+++ test/unit/Spec.hs
@@ -19,46 +19,26 @@ data RowB = RowB
   }
   deriving (Eq, Show)
 
-tableA :: [RowA]
-tableA =
-  [ RowA 1 "row 1"
-  , RowA 2 "row 2"
-  , RowA 3 "row 3"
-  , RowA 4 "row 4"
-  ]
 
-tableB :: [RowB]
-tableB =
-  [ RowB 4 1 True
-  , RowB 3 2 False
-  , RowB 2 3 True
-  , RowB 1 4 False
-  ]
-
+-- ID select: just return all rows from a table.
 selectAll :: [RowA] -> [RowA]
 selectAll table =
   runMINQ $
-    MINQ_
-      (select_ id)
-      table
+    MINQ_ (select_ id) table
 
+-- Testing select_ and where_
 selectEvens :: [RowA] -> [String]
 selectEvens table =
   runMINQ $
-    MINQ
-      (select_ aName)
-      table
-      (where_ $ even . aId)
+    MINQ (select_ aName) table (where_ $ even . aId)
 
+-- Testing where_
 selectActives :: [RowB] -> [RowB]
 selectActives table =
   runMINQ $
-    MINQ
-      (select_ id)
-      table
-      (where_ bActive)
+    MINQ (select_ id) table (where_ bActive)
 
-
+-- Testing join_
 joinTables :: [RowA] -> [RowB] -> [String]
 joinTables a b =
   runMINQ $
@@ -67,8 +47,27 @@ joinTables a b =
       (join_ a aId b bFk)
       (where_ $ bActive . snd)
 
+-- A test table
+tableA :: [RowA]
+tableA =
+  [ RowA 1 "row 1"
+  , RowA 2 "row 2"
+  , RowA 3 "row 3"
+  , RowA 4 "row 4"
+  ]
+
+-- Another test table
+tableB :: [RowB]
+tableB =
+  [ RowB 4 1 True
+  , RowB 3 2 False
+  , RowB 2 3 True
+  , RowB 1 4 False
+  ]
+
+-- Specs for select_
 spec_select =
-  describe "_select" $ do
+  describe "select_" $ do
     it "selects all rows" $
       selectAll tableA `shouldBe` tableA
     it "selects names with even id values" $
@@ -76,17 +75,21 @@ spec_select =
     it "selects active rows" $
       selectActives tableB `shouldBe` [RowB 4 1 True, RowB 2 3 True]
 
+-- Specs for join_
 spec_join =
-  describe "_join" $
+  describe "join_" $
     it "joins active rows" $
       joinTables tableA tableB `shouldBe` ["row 1", "row 3"]
 
+-- Collect all specs
 allSpecs =
   [ spec_select
   , spec_join
   ]
 
+-- Run tests
 main :: IO ()
 main = do
   specs <- concat <$> mapM testSpecs allSpecs
   defaultMain (testGroup "Specs" specs)
+
